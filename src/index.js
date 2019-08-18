eaa75af857b123ab456910c45b18f335d8a823e1
ZhengxunWu <wuzhengxun@outlook.com>
2019-08-18 23:48:10 +0800

    basic structure

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+
+const API_KEY = 'AIzaSyDE6hqjZYTfurPgIuT_ADbSFDzQVG1hsi8';
+
+
+//create a new component.
+//some html
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=>{
+            this.setState({
+                videos: videos,
+            selectedVideo:videos[0]
+        });
+        });
+    }
+
+
+    render(){
+     return (
+        <div>
+            <SearchBar />
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList 
+            onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+            videos={this.state.videos} />
+        </div>
+    );
+    }
+};
+//take this component's generated html and put it on the page(in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
